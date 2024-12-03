68730587a363d4fc3941f0b684d021e8bbdf3041
Tom Ridge <thomas.ridge@cultureamp.com>
2024-12-03 22:08:29 +1030

    day 3 initial commit

---
+++ lib/advent/year2024/day03.ex
@@ -0,0 +1,9 @@
+defmodule Advent.Year2024.Day03 do
+  def part1(args) do
+    args
+  end
+
+  def part2(args) do
+    args
+  end
+end
---
+++ test/advent/year2024/day03_test.exs
@@ -0,0 +1,21 @@
+defmodule Advent.Year2024.Day03Test do
+  use ExUnit.Case
+
+  import Elixir.Advent.Year2024.Day03
+
+  @tag :skip
+  test "part1" do
+    input = nil
+    result = part1(input)
+
+    assert result
+  end
+
+  @tag :skip
+  test "part2" do
+    input = nil
+    result = part2(input)
+
+    assert result
+  end
+end
